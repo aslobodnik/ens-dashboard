4bf0ec1449c5cbbb4fa0cc213acd40a26b2b9e59
aslobodnik <aslobodnik@gmail.com>
2024-01-24 13:17:46 -0500

    improve type for multisig

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -1,11 +1,12 @@
 import { Address } from "viem";
 
+//TODO clean up usdc Types
 export type MultiSig = {
   address: Address;
   label: string;
-  signers: string[];
-  threshold: number;
-  balance: number | BigInt;
-  usdc: number | BigInt;
-  ens: number | BigInt;
+  signers?: string[];
+  threshold?: number;
+  balance?: BigInt;
+  usdc?: BigInt;
+  ens?: BigInt;
 };
