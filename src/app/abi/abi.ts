161736ce0a4a859bf7e7bbf007cf2a9c0bc6744d
aslobodnik <aslobodnik@gmail.com>
2024-01-24 17:33:00 -0500

    add abi file

---
+++ src/app/abi/abi.ts
@@ -0,0 +1,29 @@
+export const abiBalanceOf = [
+  {
+    inputs: [{ internalType: "address", name: "account", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const;
+
+export const abiOwners = [
+  {
+    inputs: [],
+    name: "getOwners",
+    outputs: [{ internalType: "address[]", name: "", type: "address[]" }],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const;
+
+export const abiGetThreshold = [
+  {
+    inputs: [],
+    name: "getThreshold",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+];
